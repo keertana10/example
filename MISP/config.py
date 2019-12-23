265a8298f777f3e4cfaab36064613c0c7793faf1
keertana10 <55784462+keertana10@users.noreply.github.com>
2019-12-23 13:02:10 +0530

    Add files via upload

---
+++ MISP/config.py
@@ -0,0 +1,18 @@
+graph_auth = {
+    'tenant': '83a8ed06-53b4-4ea7-95e7-1fcfb93f84bc',
+    'client_id': 'bbc59f15-f909-43e1-9acb-b2e94072fbe5',
+    'client_secret': 'JdAOp?6VA5G6[0mgXmrWqn-U-i/L_j/x',
+}
+targetProduct = "Azure Sentinel"
+misp_event_filters = [
+    {
+           "eventid":'1458'
+    }
+]
+action = "alert"
+passiveOnly = False
+days_to_expire = 5
+misp_key = 'uEyCUfITmAlzK22vdiQkxiEjoH294x4rteug4iya'
+misp_domain = 'https://192.168.0.22/'
+misp_verifycert=False
+
